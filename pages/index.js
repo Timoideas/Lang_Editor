5f0e78d3f2deae2ddc249422e15d33029f6c27b3
Timoideas <Timoideas@hotmail.com>
2021-03-11 18:50:08 -0500

    Drawed

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,30 @@
 import style from "styles/css/Index.module.css";
 import { Header_Main } from "heads/Header_Main";
-import { Body, Section, Content } from "components/Resources/Timoideas";
+import {
+  Body,
+  Section,
+  Content,
+  Footer,
+  Header,
+} from "components/Resources/Timoideas";
 export default function Index() {
   return (
     <>
       <Header_Main />
       <Body>
         <Section size={1}>
-          <Content center flex={0.5}>
-            <h1 className={style.Title}>Lang Editor</h1>
-            <p className={style.Title}>by Timoideas</p>
-          </Content>
+          <Header center heigth={5} padding={0}>
+            <label className={style.Title}>Español</label>
+            <h1 className={style.Title}>Pусский</h1>
+          </Header>
+          <Section size={2}>
+            <p className={style.Title}>Lang Editor</p>
+          </Section>
+          <Footer center flex={1} row className={style.Footer}>
+            <buttom className={style.Title}>Color</buttom>
+            <buttom className={style.Title}>Keyboard</buttom>
+            <buttom className={style.Title}>Mode</buttom>
+          </Footer>
         </Section>
       </Body>
     </>
