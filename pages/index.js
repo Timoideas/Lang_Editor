8fd92a2032c811662a1d1023f3c65e160c8203a7
Timoideas <Timoideas@hotmail.com>
2021-03-11 20:38:43 -0500

    Ubicacion

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,26 @@
 import style from 'styles/css/Index.module.css';
 import { Header_Main } from 'heads/Header_Main';
-import {
-  Body,
-  Section,
-  Content,
-  Footer,
-  Header,
-} from 'components/Resources/Timoideas';
+import { Body, Section, Content } from 'components/Resources/Timoideas';
 export default function Index() {
   return (
     <>
       <Header_Main />
       <Body>
         <Section size={1}>
-          <Header center pd={0}>
-            <label className={style.Title}>Español</label>
-            <h1 className={style.Title}>Pусский</h1>
-          </Header>
-          <Section size={2}>
-            <p className={style.Title}>Lang Editor</p>
-          </Section>
-          <Footer center>
-            <buttom className={style.Title}>Color</buttom>
-            <buttom className={style.Title}>Keyboard</buttom>
-            <buttom className={style.Title}>Mode</buttom>
-          </Footer>
+          <Content center flex={1}>
+            <div className={style.Header}>
+              <label className={style.Title}>Español</label>
+              <h1 className={style.Title}>Pусский</h1>
+            </div>
+            <Content center flex={1}>
+              <p className={style.Title}>Lang Editor</p>
+            </Content>
+            <div className={style.Footer}>
+              <buttom className={style.Title}>Color</buttom>
+              <buttom className={style.Title}>Keyboard</buttom>
+              <buttom className={style.Title}>Mode</buttom>
+            </div>
+          </Content>
         </Section>
       </Body>
     </>
