2324749ddb9d7c6beeaf50aca1e51508f86537d4
Timoideas <Timoideas@hotmail.com>
2021-03-13 10:11:55 -0500

    Parrafos en div y label

--- components/Parrafo.js
+++ components/Parrafo.js
@@ -4,10 +4,24 @@ function Parrafo() {
   return (
     <Content center flex={1}>
       <div className={style.ParrafoContainer}>
-        <ul className={style.Parrafo}>
+        <div className={style.Parrafo}>
           {/* Map */}
-          <li className={style.Frase}>Привет Oлеся 👋</li>
-        </ul>
+          <label className={style.Frase}>Hola Olesya, ¿Cómo estás?</label>
+          <label className={style.Frase}>
+            If you're using Bootstrap, it has an "unstyled" class:
+          </label>
+          <label className={style.Frase}>
+            Actually, this answer is exactly what I was looking for
+          </label>
+          <label className={style.FraseI}>Привет Oлеся 👋</label>
+          <label className={style.Frase}>
+            Be aware that inline css overrules css in files
+          </label>
+          <label className={style.Frase}>
+            Este texto debería ser los uficientemente largo para ver si
+            funcionaía en párrafos grandes, muy grandes
+          </label>
+        </div>
       </div>
     </Content>
   );
