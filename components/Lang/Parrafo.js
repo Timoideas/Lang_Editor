c7ed6d5b455285535914103ef6be5f91e22a26f7
Timoideas <Timoideas@hotmail.com>
2021-03-13 11:06:44 -0500

    Paste data solved

--- components/Lang/Parrafo.js
+++ components/Lang/Parrafo.js
@@ -1,6 +1,17 @@
 import style from './Parrafo.module.css';
 import { Content } from 'components/Resources/Timoideas';
+import { useEffect, useState } from 'react';
 function Parrafo() {
+  const [PasteData, setPasteData] = useState('');
+  useEffect(() => {
+    const pasteHandler = (e) => {
+      setPasteData((e.clipboardData || window.clipboardData).getData('text'));
+    };
+    window.addEventListener('paste', pasteHandler);
+    return () => {
+      window.removeEventListener('paste', pasteHandler);
+    };
+  }, [PasteData]);
   return (
     <Content center flex={1}>
       <div className={style.ParrafoContainer}>
@@ -11,13 +22,13 @@ function Parrafo() {
         <div className={style.Parrafo}>
           {/* Map */}
           <label className={style.Frase}>Hola Olesya, ¿Cómo estás?</label>
-          <label className={style.Frase}>
-            If you're using Bootstrap, it has an "unstyled" class:
-          </label>
+          <label className={style.Frase}>'try copy and paste here'</label>
           <label className={style.Frase}>
             Actually, this answer is exactly what I was looking for
           </label>
-          <label className={style.FraseI}>Привет Oлеся 👋</label>
+          <label className={style.FraseI}>
+            Привет Oлеся 👋, {PasteData || 'Try paste your text here'}
+          </label>
           <label className={style.Frase}>
             Be aware that inline css overrules css in files
           </label>
