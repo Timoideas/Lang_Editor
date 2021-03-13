2dd6e092d135401b85c11c052ef304d0db1881ae
Timoideas <Timoideas@hotmail.com>
2021-03-13 12:46:03 -0500

    Language Picker

--- components/Lang/Header.js
+++ components/Lang/Header.js
@@ -3,6 +3,11 @@ import Emergente from 'components/Emergente';
 import style from './Header.module.css';
 function Header() {
   const [ActiveEmergente, setActiveEmergente] = useState(false);
+  const [Lang, setLang] = useState('Pусский');
+  const handlerLanguage = (lang) => {
+    setLang(lang);
+    toggleActiveEmergente();
+  };
   const toggleActiveEmergente = () => {
     setActiveEmergente(!ActiveEmergente);
   };
@@ -10,14 +15,32 @@ function Header() {
     <div className={style.Header}>
       <Emergente active={[ActiveEmergente, toggleActiveEmergente]}>
         <div className={style.Languages}>
-          <label>Español</label>
-          <label>Chino</label>
-          <label>Pусский</label>
+          <label
+            onClick={() => {
+              handlerLanguage('Español');
+            }}
+          >
+            Español
+          </label>
+          <label
+            onClick={() => {
+              handlerLanguage('Chino');
+            }}
+          >
+            Chino
+          </label>
+          <label
+            onClick={() => {
+              handlerLanguage('Pусский');
+            }}
+          >
+            Pусский
+          </label>
         </div>
       </Emergente>
       <div className={style.LanguagesSelected} onClick={toggleActiveEmergente}>
-        <label className={style.LanguageNative}>Pусский</label>
         <h2 className={style.LanguageActive}>Español</h2>
+        <label className={style.LanguageNative}>{Lang}</label>
       </div>
     </div>
   );
