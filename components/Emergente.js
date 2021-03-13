10b004326b0478c93cabd42a771ad7243c04ff45
Timoideas <Timoideas@hotmail.com>
2021-03-12 22:14:19 -0500

    Emergente component

---
+++ components/Emergente.js
@@ -0,0 +1,9 @@
+import style from './Emergente.module.css';
+function Emergente() {
+	return (
+		<div className={style.Emergente}>
+			<h2>Emergente</h2>
+		</div>
+	);
+}
+export default Emergente;
