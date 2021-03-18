e33a4c7ed786f6088c22538df4d054f197742751
Timoideas <Timoideas@hotmail.com>
2021-03-17 20:30:19 -0500

    Timoideas Component

--- components/Resources/Timoideas.js
+++ components/Resources/Timoideas.js
@@ -900,3 +900,15 @@ export function Animation({
     </div>
   );
 }
+
+export function Timoideas() {
+  return (
+    <div className='Timodieas'>
+      <img
+        alt='Timoideas Logo'
+        className='TimoideasLogo'
+        src='images/Timoideas.png'
+      />
+    </div>
+  );
+}
