0eb6d3a1c64eab5f5ec5c2fa7610fb998e69559d
Timoideas <Timoideas@hotmail.com>
2021-03-22 12:22:51 -0500

    EmergenteLanguages component

---
+++ components/EmergenteLanguages.js
@@ -0,0 +1,9 @@
+import style from './EmergenteLanguages.module.css';
+function EmergenteLanguages() {
+	return (
+		<div className={style.EmergenteLanguages}>
+			<h2>EmergenteLanguages</h2>
+		</div>
+	);
+}
+export default EmergenteLanguages;
